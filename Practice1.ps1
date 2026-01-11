f7421ba9cf85b71c09f33d6d8dc2554a0d906971
Taavi Aru <taavi.aru@voco.ee>
2026-01-11 22:18:39 +0200

    Harjutus 1

---
+++ Practice1.ps1
@@ -0,0 +1,12 @@
+﻿$list1 = @(1, 2, 3) #esimene numbrite list
+$list2 = @(4, 5, 6) #teine numbrite list
+
+$list3 = @() #kolmas list numbrite jaoks mis on hetkel tühi
+
+#liidame esimese ja teise listi liikmete summad vastavalt kokku
+#ja lisame need kolmandasse listi
+$list3 += $list1[0] + $list2[0]
+$list3 += $list1[1] + $list2[1]
+$list3 += $list1[2] + $list2[2]
+
+$list3
