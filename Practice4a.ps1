b1d670ae2d84568b36dea698a6580f197f887440
Taavi Aru <taavi.aru@voco.ee>
2026-01-11 22:21:19 +0200

    Harjutus 4a

---
+++ Practice4a.ps1
@@ -0,0 +1,13 @@
+﻿#kasutajal palutakse sisestada kaks numbrit ning siis kuvatakse neist suurim
+
+$number1 = Read-Host "Sisestage esimene number: " #esimese numbri muutuja
+$number2 = Read-Host "Sisestage teine number: " #teise numbri muutuja
+
+if($number1 -gt $number2) #võrreldakse kahte muutujat ning kuvatakse suurem number
+{
+Write-Host "Suurem number on: "$number1
+}
+else
+{
+Write-Host "Suurem number on: "$number2
+}
