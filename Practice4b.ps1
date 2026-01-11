9373b02abdecbfaf43b9f016c1242e349571d443
Taavi Aru <taavi.aru@voco.ee>
2026-01-11 22:21:54 +0200

    Harjutus 4b

---
+++ Practice4b.ps1
@@ -0,0 +1,20 @@
+﻿Write-Host "Valige riik:" -ForegroundColor Yellow
+Write-Host "1 : India" -ForegroundColor Blue
+Write-Host "2: Austraalia" -ForegroundColor Blue
+Write-Host "3: Hiina" -ForegroundColor Blue
+$riik = Read-Host "Valige üks riik"
+if($riik -eq "1")
+{
+Write-Host "India pealinn on New Delhi" -ForegroundColor Green
+}
+elseif($riik -eq "2")
+{
+Write-Host "Austraalia pealinn on Canberra" -ForegroundColor Green
+}
+elseif($riik -eq "3") {
+Write-Host "Hiina pealinn on Peking" -ForegroundColor Green
+}
+else
+{
+Write-Host "Wrong choice" -ForegroundColor Red
+}
