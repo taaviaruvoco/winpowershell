34826f078458c488c166669226b713e28be742a9
Taavi Aru <taavi.aru@voco.ee>
2026-01-11 22:20:07 +0200

    Harjutus 3

---
+++ Practice3.ps1
@@ -0,0 +1,4 @@
+﻿#skript küsib kasutaja nime ning siis kuvab saadud nime rohelise tekstiga
+
+$nimi = Read-Host "Mis on sinu nimi?: " #nime muutuja
+Write-Host "Mu nimi on: "$nimi -ForegroundColor Green
