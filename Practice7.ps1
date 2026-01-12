8403fbde2d4eaf8768c33f99f9c805ca0cc1b66f
Taavi Aru <taavi.aru@voco.ee>
2026-01-12 21:23:05 +0200

    Harjutus 7

---
+++ Practice7.ps1
@@ -0,0 +1,11 @@
+﻿$värvid = @("Red","Green","Yellow","Blue")
+
+$tabel = @()
+
+for($i = 1 ; $i-le 20 ; $i++) {
+    $number = Get-Random -Minimum 0 -Maximum 3 #saadakse juhuslik arv vahemikus 0-3, millega maaratakse varv
+    $rida = [PSCustomObject]@{ RollNumber = $i; Group = $värvid[$number] } #luuakse ajutine objekt mis lisatakse uude tabelisse
+    $tabel += $rida
+}
+
+$tabel
