92efb62f202cd84b5a36285877e1e73275198ce6
Taavi Aru <taavi.aru@voco.ee>
2026-01-12 21:23:26 +0200

    Harjutus 8

---
+++ Practice8.ps1
@@ -0,0 +1,6 @@
+﻿while (Get-Process -Name notepad -ErrorAction SilentlyContinue) {
+    Write-Host "Notepad is running"
+    Start-Sleep -Seconds 1
+}
+
+Write-Host "All Notepad instances are closed. Loop ended."
