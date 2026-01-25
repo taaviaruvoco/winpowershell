51b63978889fa71598a996d41f3d3afb13b5239a
Taavi Aru <taavi.aru@voco.ee>
2026-01-25 22:58:54 +0200

    Harjutus 9

---
+++ Practice9.ps1
@@ -0,0 +1,21 @@
+﻿#program kuvab ekraanile, et Notepad on avatud niikaua kuni koik Notepad
+#aknad on suletud, siis kuvatakse ekraanile number,  mitu korda seda teksti
+#ekraanile kuvati
+
+$luger = 0 #loeb mitu korda teksti kuvati
+do {
+  $notepad = $null
+  $notepad = Get-Process | ?{$_.name -like "note*"} #uurib kas notepad on avatud, kui muutuja on tyhi siis ei ole
+  if($notepad -ne $null) { #kui notepad on avatud
+    $avatud = 1 #notepad on avatud
+    Write-Host "Notepad is running"
+    Start-Sleep -Seconds 1
+    $luger++
+  }
+  else {
+    $avatud = 0 #notepad ei ole avatud
+  }
+}
+while($avatud -ne 0) #niikaua kui muutuja ei ole 0 tsykkel toimib
+
+$luger
